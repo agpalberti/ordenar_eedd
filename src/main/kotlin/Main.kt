22e0d48533bb3e6a61a3827fce4eff83b1d3ea1b
Alejandro González Parra <agonzalezp@iesrafaelalberti.es>
2021-12-01 16:56:41 +0100

    Función y optimización

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
-fun main() {
-
-    println("Introduce los números que quieras, separados por comas.")
-    val listaInput = readLine()?.split(',', ignoreCase = true, limit = 0)
-
-    val listaNumeros: MutableList<Int> = mutableListOf()
-
-    try {
-
-        listaInput?.forEach { listaNumeros.add(it.toInt()) }
-    } catch (_: Exception) {
-        println("La entrada no fue correcta")
-    }
+fun ordenarBurbuja(element: List<Int>): List<Int> {
+    val listaNumeros = element.toMutableList()
 
     repeat(listaNumeros.size) {
         for (i in 0 until listaNumeros.size - 1) {
@@ -22,19 +11,34 @@ fun main() {
         }
     }
 
-    println(listaNumeros)
+    return listaNumeros.toList()
+}
 
-    repeat(listaNumeros.size) {
-        for (i in 0 until listaNumeros.size - 1) {
-            if (listaNumeros[i] < listaNumeros[i + 1]) {
-                val constant = listaNumeros[i + 1]
-                listaNumeros.add(i, constant)
-                listaNumeros.removeAt(i + 2)
-            }
-        }
+
+fun main() {
+
+    println("Introduce los números que quieras, separados por comas.")
+    val listaInput = readLine()?.split(',', ignoreCase = true, limit = 0)
+
+    var listaNumeros: MutableList<Int> = mutableListOf()
+
+    try {
+
+        listaInput?.forEach { listaNumeros.add(it.toInt()) }
+    } catch (_: Exception) {
+        println("La entrada no fue correcta")
+        return
     }
 
+
+
     println(listaNumeros)
+
+    listaNumeros = ordenarBurbuja(listaNumeros).toMutableList()
+
+    println("Lista ordenada según el método de la burbuja: $listaNumeros")
+
+    println("Lista invertida :${listaNumeros.reversed()}")
 
 
 }
